8cf0925f14754c82d18f67174c4c46983f5304a8
sh4aadman <sh4adman@gmail.com>
2025-04-07 01:18:22 +0600

    creates pizza delivery server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require('cors');
+require("dotenv").config();
 const app = express();
 
 const port = process.env.port || 8000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
